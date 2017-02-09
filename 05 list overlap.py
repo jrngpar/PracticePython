1bcbd4e3343b2fc78979d3a515a9e37722f0d9a6
jrngpar <jrngpar@gmail.com>
2017-02-08 16:03:57 -0800

    Update 05 list overlap.py
    Added lists
    printed list_A

--- 05 list overlap.py
+++ 05 list overlap.py
@@ -1,13 +1,18 @@
+import random
+
 length_A = input("Length of 1st list:")
 length_B = input("Length of 2nd list:")
 
 length_A = int(length_A)
 length_B = int(length_B)
 
+list_A = []
+list_B = []
+
 print(length_A)
 print(length_B)
 
-for x in range(0,length_A):
+for x in range(0,length_A+1):
     list_A.append(random.randint(0,99))
   
-print(x)
+print(list_A)
