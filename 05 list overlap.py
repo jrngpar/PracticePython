d5365c06b293ddea00855aec4bbf32e69c0b98a8
jrngpar <jrngpar@gmail.com>
2017-02-09 09:16:07 -0800

    Update 05 list overlap.py

--- 05 list overlap.py
+++ 05 list overlap.py
@@ -8,15 +8,28 @@ length_B = int(length_B)
 
 list_A = []
 list_B = []
+list_same=[]
 
 #print(length_A)
 #print(length_B)
+def list_overlap(l1,l2):
+    for x in range(0,length_A):
+        list_A.append(random.randint(0,99))
 
-for x in range(0,length_A):
-    list_A.append(random.randint(0,99))
-    
-for x in range(0,length_B):
-    list_B.append(random.randint(0,99))
-  
-print(list_A)
-print(list_B)
+    for x in range(0,length_B):
+        list_B.append(random.randint(0,99))
+
+    print(list_A)
+    print(list_B)
+
+    for element_A in list_A:
+      a = element_A
+      for element_B in list_B:
+        b = element_B
+        if a==b:
+          list_same.append(a)
+
+    print(list_same)
+
+list_overlap(list_A,list_B)
+
