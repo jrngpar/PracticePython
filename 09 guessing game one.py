dd22e1b0f89997177d31787f8646e38f9ff87f74
jrngpar <jrngpar@gmail.com>
2017-02-17 15:14:13 -0800

    Update 09 guessing game one.py
    Checks guess against random number

--- 09 guessing game one.py
+++ 09 guessing game one.py
@@ -6,3 +6,12 @@ print(number)
 guess = input("Guess the number from 1-9:")
 guess = int(guess)
 print(guess)
+
+if number==guess:
+  print("Same")
+  
+if number<guess:
+  print("Guess is larger")
+  
+if number>guess:
+  print("Guess is smaller")
