eabcf4b1ee0b2b9cb51413683a43f5b9e0e294e7
Jonathan <jrngpar@gmail.com>
2017-02-22 01:45:34 -0800

    Got guessing game right

--- 09 guessing game one.py
+++ 09 guessing game one.py
@@ -1,20 +1,36 @@
 import random
 
+guess=-1
 number = random.randint(1,9)
-print(number)
-
-guess = input("Guess the number from 1-9:")
-guess = int(guess)
-print(guess)
+#print(number)
+num_guess=0
+def get_guess():
+	global guess
+	guess = input("Guess the number from 1-9:")
+	guess = int(guess)
+	print(guess)
 
 def checker():
-  if number==guess:
-    print("Same")
-  
-  if number<guess:
-    print("Guess is larger")
-  
-  if number>guess:
-    print("Guess is smaller")
+	global num_guess
+	if number==guess:
+		print("Good guess")
+		num_guess += 1
+		print("It took you {} guesses to get the right answer!num_guess)
 
+
+	if number<guess:
+		print("Guess is larger\nTry again")
+		num_guess +=1
+		get_guess()
+		checker()
+		
+  
+	if number>guess:
+		print("Guess is smaller\nTry again")
+		num_guess+=1
+		get_guess()
+		checker()
+		
+
+get_guess()
 checker()
