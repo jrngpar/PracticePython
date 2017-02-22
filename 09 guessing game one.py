2cea6ad52b204471da988f2b990d6a33914a3086
jrngpar <jrngpar@gmail.com>
2017-02-22 10:08:53 -0800

    Update 09 guessing game one.py
    change print statements

--- 09 guessing game one.py
+++ 09 guessing game one.py
@@ -1,36 +1,37 @@
 import random
 
-guess=-1
-number = random.randint(1,9)
-#print(number)
-num_guess=0
-def get_guess():
-	global guess
-	guess = input("Guess the number from 1-9:")
-	guess = int(guess)
-	print(guess)
 
-def checker():
-	global num_guess
-	if number==guess:
-		print("Good guess")
-		num_guess += 1
-		print("It took you {} guesses to get the right answer!".format(num_guess))
-
-
-	if number<guess:
-		print("Guess is larger\nTry again")
-		num_guess +=1
-		get_guess()
-		checker()
-		
+  guess=-1
+  number = random.randint(1,9)
+  #print(number)
+  num_guess=0
+  def get_guess():
+  	global guess
+  	guess = input("Guess the number from 1-9:")
+  	guess = int(guess)
+  	print(guess)
   
-	if number>guess:
-		print("Guess is smaller\nTry again")
-		num_guess+=1
-		get_guess()
-		checker()
-		
-
-get_guess()
-checker()
+  def checker():
+  	global num_guess
+  	if number==guess:
+  		print("Good guess")
+  		num_guess += 1
+  		print("It took you {} guesses to get the right answer!".format(num_guess))
+  
+  
+  	if number<guess:
+  		print("Your guess is larger\nTry again")
+  		num_guess +=1
+  		get_guess()
+  		checker()
+  		
+    
+  	if number>guess:
+  		print("Your guess is smaller\nTry again")
+  		num_guess+=1
+  		get_guess()
+  		checker()
+  		
+  
+  get_guess()
+  checker()
