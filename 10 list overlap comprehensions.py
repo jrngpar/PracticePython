99624c8c63fc235dddc80c6abaae23222b82a990
jrngpar <jrngpar@gmail.com>
2017-03-02 13:15:58 -0800

    Update 10 list overlap comprehensions.py
    merge lists and remove duplicates

--- 10 list overlap comprehensions.py
+++ 10 list overlap comprehensions.py
@@ -3,14 +3,24 @@ import random
 ls_A = []
 ls_B = []
 
-length = 5
+#length = 5
+length1 = input("How long for list 1?\n")
+length1 = int(length1)
+length2 = input("list 2?\n")
+length2 = int(length2)
 
-for i in range(length):
+for i in range(length1):
     ls_A.append(random.randint(0,9))
     
 print (ls_A)
 
-for i in range(length):
+for i in range(length2):
     ls_B.append(random.randint(0,9))
     
 print (ls_B)
+
+common = []
+common = [x for x in ls_A if x in ls_B and x not in common]
+print(common)
+common = set(common)
+print(common)
