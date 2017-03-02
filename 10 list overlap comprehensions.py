8dd4145189d376bb42b2471ce59b516f9016b44e
jrngpar <jrngpar@gmail.com>
2017-03-01 16:34:58 -0800

    Update 10 list overlap comprehensions.py
    create lists with program defined lengths

--- 10 list overlap comprehensions.py
+++ 10 list overlap comprehensions.py
@@ -1,1 +1,16 @@
 import random
+
+ls_A = []
+ls_B = []
+
+length = 5
+
+for i in range(length):
+    ls_A.append(random.randint(0,9))
+    
+print (ls_A)
+
+for i in range(length):
+    ls_B.append(random.randint(0,9))
+    
+print (ls_B)
