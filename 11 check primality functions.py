f5df7c123eefa59c03f05cb4219eeb909f9721cc
jrngpar <jrngpar@gmail.com>
2017-03-02 16:05:55 -0800

    Update 11 check primality functions.py

--- 11 check primality functions.py
+++ 11 check primality functions.py
@@ -1,5 +1,18 @@
 def get_int():
-    return int(input("Number?\n"))
+    return int(input("Enter number to check if prime:\n"))
 
-num = get_int()
-print(num)
+am_i_prime = get_int()
+
+def check_divisors(my_int):
+    my_num=my_int
+    my_range=(1,my_int+1)
+    for x in my_range:
+        if (my_num%x==0):
+            print("Number isn't prime")
+            break
+        else:
+            print("Prime")
+        
+check_divisors(am_i_prime)
+
+#need to exclude 1 and the number
