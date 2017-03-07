7f44796902d58c2de42f21eb247c11ed85c9ac26
Jonathan <jrngpar@gmail.com>
2017-03-07 14:55:14 -0500

    Works so far

--- 11 check primality functions.py
+++ 11 check primality functions.py
@@ -5,13 +5,14 @@ am_i_prime = get_int()
 
 def check_divisors(my_int):
     my_num=my_int
-    my_range=(1,my_int+1)
+    my_range=(2,(my_int/2)+1)
     for x in my_range:
         if (my_num%x==0):
             print("Number isn't prime")
             break
         else:
             print("Prime")
+            break
         
 check_divisors(am_i_prime)
 
