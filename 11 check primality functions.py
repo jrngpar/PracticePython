3417c0e67d4babcb1e962eb3b6273f1ad319b119
Jonathan <jrngpar@gmail.com>
2017-03-07 15:02:09 -0500

    1 and 2 are prime

--- 11 check primality functions.py
+++ 11 check primality functions.py
@@ -1,19 +1,25 @@
 def get_int():
-    return int(input("Enter number to check if prime:\n"))
+	return int(input("Enter number to check if prime:\n"))
 
 am_i_prime = get_int()
 
 def check_divisors(my_int):
-    my_num=my_int
-    my_range=(2,(my_int/2)+1)
-    for x in my_range:
-        if (my_num%x==0):
-            print("Number isn't prime")
-            break
-        else:
-            print("Prime")
-            break
-        
+	my_num=my_int
+	my_range=(2,(my_int/2)+1)
+	for x in my_range:
+		if my_num==1:
+			print("Prime")
+			break
+		elif my_num==2:
+			print("Prime")
+			break
+		elif (my_num%x==0):
+			print("Number isn't prime")
+			break
+		else:
+			print("Prime")
+			break
+		
 check_divisors(am_i_prime)
 
-#need to exclude 1 and the number
+#need to exclude 1 and 2
