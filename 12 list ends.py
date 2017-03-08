6be8d8c25fea8d371fbd2d0554340f8d87b99741
Jonathan <jrngpar@gmail.com>
2017-03-08 10:26:14 -0500

    Create random list, need make into function

--- 12 list ends.py
+++ 12 list ends.py
@@ -1,9 +1,24 @@
 #new project!
-my_list = [1,2,4,5,76,4,2]
+import random
+
+rand_list = []
+
+user_length = input("List length?: ")
+
+print(user_length)
+
+for x in range(0,int(user_length)):
+	rand_list.append(random.randint(0 ,100))
+
+print(rand_list)
+
+#rand_list
+
+my_list = [1,2,4,5,76,4,2,0]
 
 new_list = []
 
-new_list.append(my_list[0])
-#my_list = my_list.reverse()
-new_list.append(my_list[0])
+new_list.append(rand_list[0])
+rand_list.reverse()
+new_list.append(rand_list[0])
 print(new_list)
