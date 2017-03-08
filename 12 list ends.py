b2d734dbe35b5131accc0910e8da48c4faa0380e
Jonathan <jrngpar@gmail.com>
2017-03-08 10:31:09 -0500

    Create function to reverse list and return the first and last items

--- 12 list ends.py
+++ 12 list ends.py
@@ -14,11 +14,21 @@ print(rand_list)
 
 #rand_list
 
-my_list = [1,2,4,5,76,4,2,0]
+# my_list = [1,2,4,5,76,4,2,0]
 
-new_list = []
+# new_list = []
 
-new_list.append(rand_list[0])
-rand_list.reverse()
-new_list.append(rand_list[0])
-print(new_list)
+# new_list.append(rand_list[0])
+# rand_list.reverse()
+# new_list.append(rand_list[0])
+# print(new_list)
+
+def get_firstandlast(in_list):
+	a = in_list
+	new_list=[]
+	new_list.append(a[0])
+	reverse=a[::-1]
+	new_list.append(reverse[0])
+	return new_list
+
+print(get_firstandlast(rand_list))
