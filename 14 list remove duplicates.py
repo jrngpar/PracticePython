25d3c6a34bdc249a851d37b42faa6cb581d8d9f7
Jonathan <jrngpar@gmail.com>
2017-03-08 12:52:50 -0500

    loops for each item in the list

--- 14 list remove duplicates.py
+++ 14 list remove duplicates.py
@@ -1,5 +1,6 @@
 #removes duplicates
 #check with a loop and also check with a set transformation
+#check user input, ask how to sort
 import random
 
 
@@ -15,11 +16,17 @@ def create_list(list_length):
 
 check_list = create_list(get_list_len())
 
-#def remove_dup_with_loop(in_list):
-
+def remove_dup_with_loop(input_list):
+	no_duplicates = input_list
+	for x in range(0,len(input_list)):
+		print("it printed")
+	return no_duplicates
 
 def remove_dup_with_set(in_list):
 	make_set = set(in_list)
 	return make_set
+
 print (check_list)
 print (remove_dup_with_set(check_list))
+
+print (remove_dup_with_loop(check_list))
