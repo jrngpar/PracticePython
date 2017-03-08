0d52e9f298663e993388aeb744c86549f7d85f65
Jonathan <jrngpar@gmail.com>
2017-03-08 13:23:11 -0500

    start to write the reverse string function

--- 15 reverse word order.py
+++ 15 reverse word order.py
@@ -1,1 +1,10 @@
 #reverse word order
+#ask for a long string with multiple words
+#print it back with the words in backwards order
+
+#remove spaces? Maybe print back with words in reverse
+#2 functions, one to reverse order of words, one to reverse letters in words?
+#Can call both functions to reverse order and letters if wanted
+
+def reverse_words(sentence_in):
+	
