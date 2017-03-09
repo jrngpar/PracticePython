5fdde3e843f5dea69fec3e23a124f45a166cff33
jrngpar <jrngpar@gmail.com>
2017-03-09 13:23:58 -0800

    Update 15 reverse word order.py
    Add function to reverse word order

--- 15 reverse word order.py
+++ 15 reverse word order.py
@@ -7,4 +7,12 @@
 #Can call both functions to reverse order and letters if wanted
 
 def reverse_words(sentence_in):
-	
+	swapped = sentence_in.split(" ")
+	swapped = " ".join(reversed(swapped))
+	return swapped
+
+def get_sentence():
+	user_sentence = input("What would you like reversed:\n")
+	return user_sentence
+
+print(reverse_words(get_sentence()))
