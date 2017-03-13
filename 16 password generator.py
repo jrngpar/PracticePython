f920306bb9e38263b337decaa02138a6962f7b7f
jrngpar <jrngpar@gmail.com>
2017-03-13 12:24:16 -0700

    Update 16 password generator.py
    will it blend?

--- 16 password generator.py
+++ 16 password generator.py
@@ -6,24 +6,38 @@
 
 #use random numbers. If random number is in a range, create one digit out of a specific set of characters(numbers, letters, symbols)
 #ask user for password length and loop to make that length
+import random
 
 def get_parameters():
     generator_input = input("Want to make a password?\nEnter 'quit' to quit: ")
     if generator_input != "quit":
-        get_strength()
+        make_password(get_length())
                 
-def get_strength():
-        generator_input = input("How strong?\n(1)Strong\n(2)Average\n(3)Weak\n")
-        if generator_input == "1":
-            print("strong")
             
-        elif generator_input == "2":
-            print("average")
-            
-        elif generator_input == "3":
-            print("weak")
-            
-        else:
-            print("Please enter a valid selection")
-            get_strength()
-get_parameters()        
+def make_password(length):
+    password = []
+    generator_input = input("How strong?\n(1)Strong\n(2)Average\n(3)Weak\n")
+    if generator_input == "1":
+        for x in range(0,length):
+            password.append(gen_symbol())
+    elif generator_input == "2":
+        print("average")
+    elif generator_input == "3":
+        print("weak")
+    else:
+        print("Please enter a valid selection")
+        get_strength()
+    
+    print("".join(password))
+    
+    
+def gen_symbol():
+    symbol_list = ["!","@","%","^","&","/","(",")",":","~",";"]
+    rand_symbol = random.choice(symbol_list)
+    return (rand_symbol)
+    
+    
+def get_length():
+    pass_length = input("How many characters:")
+    return int(pass_length)
+get_parameters()
